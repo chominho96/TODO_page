68319f5069649d5393210748b1c1401680007489
CHO Min Ho <chominho96@inha.edu>
2022-02-18 22:37:40 +0900

    Add clock Service
    1. Add clock service

---
+++ js/clock.js
@@ -0,0 +1,29 @@
+const clock = document.querySelector(".clock");
+
+function clockUpdate() {
+  // update clock every second
+  const time = new Date();
+
+  let tempHour = time.getHours();
+  let AM_PM = "AM";
+
+  // provide AM/PM
+  if (tempHour >= 12) {
+    // if it is PM
+    AM_PM = "PM";
+    if (tempHour !== 12) {
+      // EXCEPTION : 12 PM should be 12
+      tempHour -= 12;
+    }
+  }
+
+  const hour = String(tempHour).padStart(2, "0");
+  const minute = String(time.getMinutes()).padStart(2, "0");
+  const second = String(time.getSeconds()).padStart(2, "0");
+
+  // set time
+  clock.innerText = `${hour} : ${minute} : ${second} ${AM_PM}`;
+}
+
+clockUpdate();
+setInterval(clockUpdate, 1000);
